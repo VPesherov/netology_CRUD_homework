3f2b0d73a4ab2b35d44016894aa93469986e607f
VPesherov <vyacheslav.pesherov@yandex.ru>
2023-10-30 10:13:05 +0700

    reded homework

--- smart_home/measurement/urls.py
+++ smart_home/measurement/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import SensorView, MeasureView
+from .views import SensorView, MeasureView, SensorsView
 
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
-    path('sensors/', SensorView.as_view()),
+    path('sensors/', SensorsView.as_view()),
     path('sensors/<int:pk>/', SensorView.as_view()),
     path('measurements/', MeasureView.as_view()),
 
--- smart_home/measurement/views.py
+++ smart_home/measurement/views.py
@@ -18,38 +18,23 @@ from .serializers import SensorSerializers, MeasureSerializers
 #     return Response(ser.data)  # специальный класс который возвращает словарь
 
 class SensorView(APIView):
-    def get(self, request, pk=None):
-        if pk:
-            sensors = Sensor.objects.get(pk=pk)
-            measurement = Measurement.objects.filter(id_sensor=pk)
-            ser_measure = MeasureSerializers(measurement, many=True)
-            # print(sensors.id)
-            result_data = {
-                "id": sensors.id,
-                "name": sensors.name,
-                "description": sensors.description,
-                "measurements": ser_measure.data
-            }
-            return Response(result_data)  # специальный класс который возвращает словарь
-
-        else:
-            sensors = Sensor.objects.all()  # Достанем все оружия из нашей модели
-            ser = SensorSerializers(sensors, many=True)
-            return Response(ser.data)  # специальный класс который возвращает словарь
-
-    def post(self, request):
-
-        sensor = SensorSerializers(data=request.data)
-
-        if sensor.is_valid():
-            sensor.save()
-            return Response(sensor.data)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    def get(self, request, pk):
+        sensors = Sensor.objects.get(pk=pk)
+        measurement = Measurement.objects.filter(id_sensor=pk)
+        ser_measure = MeasureSerializers(measurement, many=True)
+        # print(sensors.id)
+        result_data = {
+            "id": sensors.id,
+            "name": sensors.name,
+            "description": sensors.description,
+            "measurements": ser_measure.data
+        }
+        return Response(result_data)  # специальный класс который возвращает словарь
 
     def patch(self, request, pk):
         sensor = Sensor.objects.get(pk=pk)
-        serializer = SensorSerializers(sensor, data=request.data, partial=True) # set partial=True to update a data partially
+        serializer = SensorSerializers(sensor, data=request.data,
+                                       partial=True)  # set partial=True to update a data partially
         if serializer.is_valid():
             serializer.save()
             return Response(data=serializer.data)
@@ -66,3 +51,21 @@ class MeasureView(APIView):
             return Response(measure.data)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class SensorsView(APIView):
+
+    def post(self, request):
+
+        sensor = SensorSerializers(data=request.data)
+
+        if sensor.is_valid():
+            sensor.save()
+            return Response(sensor.data)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request):
+        sensors = Sensor.objects.all()
+        ser = SensorSerializers(sensors, many=True)
+        return Response(ser.data)  # специальный класс который возвращает словарь
